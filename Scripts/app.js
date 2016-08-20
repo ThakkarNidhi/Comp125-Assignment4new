68469c6450828806b4f3ac3b32140b45c020ed2b
NidhiThakkar <thakkarnidhi20@outlook.com>
2016-08-19 22:08:58 -0400

    final commit

---
+++ Scripts/app.js
@@ -0,0 +1,12 @@
+/* main JavaScript file */
+// IIFE - Immediately Invoked Function Expression
+/**
+*filename: app.js
+*
+@author Nidhi Thakkar
+@date 19/08/2016
+*
+*student id:300872151
+*website  :http://comp125assignment4.azurewebsites.net/
+@description This is the main java script file for the website.
+*/
